dc4edf38cb0306b93370b3561e1cdca16bb42278
ANAMIKA SANILKUMAR <anamikasanilkumar36@gmail.com>
2026-04-19 22:38:44 +0530

    initial portfolio upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+    document.body.classList.toggle("light");
+}
+
+const elements = document.querySelectorAll(".reveal");
+
+function showOnScroll() {
+    elements.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            el.classList.add("show");
+        }
+    });
+}
+
+window.addEventListener("scroll", showOnScroll);
+showOnScroll();
